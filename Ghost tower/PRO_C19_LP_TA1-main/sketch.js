ffcd21af1ba79a2e4f92d2594b5ba68161164877
CodingClassstudent <112195659+CodingClassstudent@users.noreply.github.com>
2022-10-26 18:08:32 -0400

    Add files via upload

---
+++ Ghost tower/PRO_C19_LP_TA1-main/sketch.js
@@ -0,0 +1,68 @@
+var towerImg, tower;
+var doorImg, door, doorsGroup;
+var climberImg, climber, climbersGroup;
+var ghost, ghostImg;
+var invisibleBlockGroup, invisibleBlock;
+var gameState = "play"
+
+function preload(){
+  towerImg = loadImage("tower.png");
+  doorImg = loadImage("door.png");
+  climberImg = loadImage("climber.png");
+  ghostImg = loadImage("ghost-standing.png");
+  spookySound = loadSound("spooky.wav");
+}
+
+function setup(){
+  createCanvas(600,600);
+  spookySound.loop();
+  tower = createSprite(300,300);
+  tower.addImage("tower",towerImg);
+  tower.velocityY = 1;
+  
+  doorsGroup = new Group();
+  climbersGroup = new Group();
+  invisibleBlockGroup = new Group();
+  
+  ghost = createSprite(200,200,50,50);
+  ghost.scale = 0.3;
+  ghost.addImage("ghost", ghostImg);
+}
+
+function draw(){
+  background(0);
+  if(gameState="play"){
+
+    if(keyDown("left_arrow")){
+    ghost.x=ghost.x+-3
+    }
+    if(keyDown("right_arrow")){
+      ghost.x=ghost.x+3
+    }
+    if(keyDown("space")){
+      ghost.y=ghost.y-3
+    }
+    
+
+    
+  }
+    
+    drawSprites();
+  }
+  
+  if (gameState === "end"){
+    stroke("yellow");
+    fill("yellow");
+    textSize(30);
+    text("Game Over", 230,250)
+  }
+
+
+
+function spawnDoors() {
+  //write code here to spawn the doors in the tower
+  if (frameCount % 240 === 0) {
+    
+  }
+}
+
